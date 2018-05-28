59a8b459dbcac587f71cc522bedf5d9148901f22
Andrea <andrea.zago@medas-solutions.it>
2018-05-28 17:01:12 +0200

    Modifiche nuova interfaccia

---
+++ js/code.js
@@ -0,0 +1,22 @@
+/**
+ *  Get url parameter to search directly an user
+ */
+
+const getUrlParameter = function getUrlParameter(sParam) {
+    let sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
+
+const playerId = getUrlParameter('playerId');
+
+console.log(playerId);
